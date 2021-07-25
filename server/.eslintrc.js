9d4c055998ca6a84593a4313c633837de79e4185
bearbearbear <bear-bear-bear@kakao.com>
2021-07-26 02:24:19 +0900

    fix: prettier 설정이 적용되지 않던 문제 수정

---
+++ server/.eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [
+    'airbnb-typescript/base',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+    ecmaFeatures: { jsx: false },
+  },
+  env: {
+    browser: false,
+    node: true,
+  },
+  ignorePatterns: ['node_modules', 'dist'],
+  rules: {
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    'no-console': 'warn',
+    'import/no-cycle': 'off',
+    'no-shadow': 'off', // 아래 @typescript-eslint/no-shadow 과의 충돌을 피하기 위해 off 처리
+    '@typescript-eslint/no-shadow': ['error'],
+    'no-use-before-define': ['error', { functions: true, classes: true }],
+  },
+};
