687271fb505fde20509ff9da876ef4618c5b236a
bearbearbear <bear-bear-bear@kakao.com>
2021-07-27 19:50:59 +0900

    feat: koa 초기 설정 추가

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,13 @@
-console.log('test file');
+import Koa from 'koa';
+import Router from 'koa-router';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = new Koa();
+const router = new Router();
+
+app.use(router.routes());
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is listening to port ${process.env.PORT}`);
+});
