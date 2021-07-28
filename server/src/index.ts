5f8b0102990b6e198329db85fad345add47c8d17
bearbearbear <bear-bear-bear@kakao.com>
2021-07-29 00:05:32 +0900

    feat: 백단 스노우팩 제거

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,12 +2,9 @@ import Koa from 'koa';
 import Router from 'koa-router';
 import dotenv from 'dotenv';
 
-import startSnowpackServer from '@lib/startSnowpackServer';
-
 dotenv.config();
 const app = new Koa();
 const router = new Router();
-const server = await startSnowpackServer();
 
 // default error handler (수정 예정)
 app.use(async (ctx, next) => {
@@ -26,11 +23,6 @@ app.use(async (ctx, next) => {
 });
 
 app.use(router.routes());
-app.use(async (ctx, next) => {
-  const buildResult = await server.loadUrl(ctx.request.url);
-  ctx.body = buildResult?.contents;
-  next();
-});
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is listening to port ${process.env.PORT}`);
