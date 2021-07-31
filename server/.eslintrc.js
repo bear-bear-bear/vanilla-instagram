87ab29e1f7308f94e9e9b4fdd627c6871d3bf94c
bearbearbear <bear-bear-bear@kakao.com>
2021-07-31 21:15:34 +0900

    fix: eslint 설정 오류 수정

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -9,7 +9,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint', 'prettier'],
   extends: [
-    'airbnb-base',
+    'airbnb-typescript/base',
     'plugin:import/errors',
     'plugin:import/warnings',
     'plugin:@typescript-eslint/recommended',
