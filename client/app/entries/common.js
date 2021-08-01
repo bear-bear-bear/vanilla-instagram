154a5c7f76cb60481ffdf5a29a2f43ba0752af03
Kim June Jae <bear-bear-bear@kakao.com>
2021-08-01 16:33:03 +0900

    feat: 클라이언트 번들과 koa 연결, 정적 환경에서 hmr 과 유사한 효과를 낼 수 있게 해주는 미들웨어 작성 (#14)
    * fix: tsconfig target, module 수정
    
    * feat: 스크립트 수정
    
    * feat: 디펜던시 에러 수정, koa-static 설치
    
    * feat: client/app/ 에 대한 alias 추가
    
    * feat: koa 서버 기반 개발 환경에서 클라이언트단 변경 사항에 대해 hmr과 유사한 효과를 낼 수 있는 기능을 SSE로 구현
    
    * test: 테스트 pug 파일 title 변경
    
    * test: 테스트 파일 폰트사이즈 조정
    
    * feat: 백단 핫 리로딩 코드를 production이 아닐 때만 동작하도록 변경
    
    * feat: development 기능에 사용되는 모듈을 dynamic import 처리

--- client/app/entries/common.js
+++ client/app/entries/common.js
@@ -1,8 +1,31 @@
 import '@scripts/common/common';
 import '@styles/common/common.scss';
 
-// dev-server 에서 pug 변경에 대한 HMR 지원
+/**
+ * @desc dev-server 에서 pug 변경에 대한 HMR 지원
+ */
+const watchPugs = async () => {
+  const { default: pageInfos } = await import('../../page.config');
+
+  pageInfos.forEach(({ pug: pugPath }) => {
+    import(`@/${pugPath}`);
+  });
+};
+
+/**
+ * @desc 
+ * koa.js (server) 에서 번들 결과물들의 변경을 감지하여 신호를 보내주면, 해당 신호를 감지하여 페이지 리로드. 
+  koa server로 정적이라 할 수 있는 번들 결과물들을 제공하면서도, hmr 과 유사한 효과를 낼 수 있게 해줌.
+ */
+const detectSSE = (callback) => {
+  // const source = new EventSource(`http://localhost:${SERVER_PORT}/sse`);
+  const source = new EventSource(`http://localhost:8001/sse`);
+  source.onmessage = () => {
+    callback();
+  };
+};
+
 if (process.env.NODE_ENV !== 'production') {
-  import('@pages/test.pug');
-  import('@pages/index.pug');
+  watchPugs();
+  detectSSE(() => window.location.reload());
 }
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,73 @@
+import path from 'path';
+import fs from 'fs';
+
 import Koa from 'koa';
 import Router from 'koa-router';
 import dotenv from 'dotenv';
+import serve from 'koa-static';
 
 dotenv.config();
 const app = new Koa();
 const router = new Router();
 
-// default error handler (수정 예정)
+const STATIC_DIR = path.join(__dirname, 'public');
+
+app.use(router.routes());
+app.use(serve(STATIC_DIR));
+
+/**
+ * @desc
+ * hmr 과 유사한 효과를 낼 수 있게 해주는 미들웨어 (코드 분할 예정)
+ * 클라이언트단 번들 결과물 변경시 SSE 를 통해 브라우저에 신호를 전송하여 리로드하게 합니다. (신호 감지 후 리로드하는 로직은 클라이언트 쪽에 있습니다)
+
+  원리를 정리하면 아래와 같음
+  1. 프론트엔드 작업 공간에서 무언가 코드를 작성
+  2. 프론트엔드측 webpack watch 모드가 변경을 감지하여 리빌드
+  3. koa 서버에서 빌드 디렉터리의 변경을 감지하여 브라우저에 신호 보냄 (SSE)
+  4. 해당 신호를 client/app/entries/common.js 에서 해당 신호를 받아 브라우저 리로딩  (리로딩하는 이유 - 서버에 데이터 재요청을 보내게 되어 변경된 데이터가 적용될 수 있게 함)
+ */
+if (process.env.NODE_ENV !== 'production') {
+  app.use(async (ctx, next) => {
+    if (ctx.path !== '/sse') {
+      await next();
+      return;
+    }
+
+    ctx.request.socket.setTimeout(0);
+    ctx.req.socket.setNoDelay(true);
+    ctx.req.socket.setKeepAlive(true);
+
+    ctx.set({
+      'Content-Type': 'text/event-stream',
+      'Cache-Control': 'no-cache',
+      Connection: 'keep-alive',
+    });
+
+    const { PassThrough } = await import('stream');
+    const stream = new PassThrough();
+
+    ctx.status = 200;
+    ctx.body = stream;
+
+    const toSSEStream = (str: string) => `data: ${str}\n\n`;
+
+    // fs.watch의 이중실행 이슈를 방지하기 위한 쓰로틀링
+    let watching: ReturnType<typeof setTimeout>;
+    fs.watch(STATIC_DIR, undefined, (event) => {
+      if (watching) {
+        clearTimeout(watching);
+      }
+      watching = setTimeout(() => {
+        stream.write(toSSEStream(event)); // 번들 파일 변경시 브라우저로 SSE
+        clearTimeout(watching);
+      }, 200);
+    });
+  });
+}
+
+/**
+ * @desc default error handler (내부 코드 수정 및 분할 예정)
+ */
 app.use(async (ctx, next) => {
   try {
     await next();
@@ -22,8 +83,15 @@ app.use(async (ctx, next) => {
   }
 });
 
-app.use(router.routes());
+/**
+ * @desc default success handler (내부 코드 수정 및 분할 예정)
+ */
+app.use((ctx) => {
+  ctx.status = 200;
+  ctx.body = 'ok';
+});
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is listening to port ${process.env.PORT}`);
+const PORT = process.env.PORT || 8001;
+app.listen(PORT, () => {
+  console.log(`Server is listening to ✨ http://localhost:${PORT} ✨`);
 });
