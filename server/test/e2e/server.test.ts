f4e6e37ab9bb4161034606d0b653bbd7e892297d
최은기 <galaxyhi4276@gmail.com>
2021-08-10 14:27:30 +0900

    fix: test 종료 안되는 문제 해결

--- server/test/e2e/server.test.ts
+++ server/test/e2e/server.test.ts
@@ -9,6 +9,10 @@ describe('서버 실행 환경 테스트', () => {
     server = app.listen(8080);
   });
 
+  afterAll(() => {
+    server.close();
+  });
+
   it('서버가 정상적으로 작동한다.', async () => {
     await request(server)
       .head('/')
