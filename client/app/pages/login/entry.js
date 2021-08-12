7effa9ebb54c7bcf11c506982c3a9a1717197cce
baekDongJae <pandora2948@naver.com>
2021-08-12 18:32:28 +0900

    conflict resolve

--- client/app/pages/login/entry.js
+++ client/app/pages/login/entry.js
@@ -1,6 +1,6 @@
 import '@templates/auth-form/_entry';
 import '@templates/login-iphone/_entry';
-// import '@templates/legal-policy/_entry';
+import '@templates/legal-policy/_entry';
 
 import './login';
 import './login.scss';
