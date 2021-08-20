178e2b26f024e5afeb4de44bb445fe1c421c7118
bearbearbear <ghlwkdthfl@gmail.com>
2021-08-21 02:18:08 +0900

    fix: 테스트 오류 임시 해결

--- server/test/e2e/server.test.ts
+++ server/test/e2e/server.test.ts
@@ -8,6 +8,7 @@ describe('서버 실행 환경 테스트',  () => {
   });
 
   test('Example route test', async () => {
+    jest.useFakeTimers(); // FIXME: 임시 테스트 오류 해결  (ReferenceError: You are trying to `import` a file after the Jest environment has been torn down.)
     const response = await request(app.callback()).get('/');
 
     expect(response).toBeDefined();
