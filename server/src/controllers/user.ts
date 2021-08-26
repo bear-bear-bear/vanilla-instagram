5fcef3c886e613a73f320b9616d9f89fd6441061
bearbearbear <bear-bear-bear@kakao.com>
2021-08-27 00:09:58 +0900

    feat: controller들의 모델 메서드 call과 상태코드 반환값에 관한 유닛 테스트 작성

--- server/src/controllers/user.ts
+++ server/src/controllers/user.ts
@@ -1,7 +1,9 @@
 import type { Context, Next } from 'koa';
 
-export const verifyExistenceUsername = () => {};
+export const verifyExistenceUsername = (ctx: Context, next: Next) => {};
 
-export const sendSMSVerificationCode = () => {};
+export const sendSMSVerificationCode = (ctx: Context, next: Next) => {};
+
+export const checkSMSVerificationCode = (ctx: Context, next: Next) => {};
 
 export const createUser = (ctx: Context, next: Next) => {};
--- server/test/unit/user.test.ts
+++ server/test/unit/user.test.ts
@@ -1,8 +1,18 @@
-import { Context, Next } from 'koa';
+import type { Context, Next } from 'koa';
 import { createMockContext } from '@shopify/jest-koa-mocks';
  
-import { createUser } from 'app/controllers/user';
-import User from 'app/models/user';
+import {
+  verifyExistenceUsername,
+  sendSMSVerificationCode,
+  checkSMSVerificationCode,
+  createUser,
+} from 'src/controllers/user';
+import User from 'src/models/user';
+import mockJoinFields from '../mock-data/join-fields.json';
+
+// mock
+User.create = jest.fn();
+User.findAll = jest.fn();
 
 let ctx: Context;
 let next: Next;
@@ -12,21 +22,58 @@ beforeEach(() => {
 });
 
 describe('Create User Controllers', () => {
-  describe('createUser', () => {
-    it('함수여야한다.', () => {
-      expect(typeof createUser).toBe('function');
-    });
-
-    it('User 모델의 create 메서드를 호출해야 한다.', async () => {
-      await createUser(ctx, next);
-    });
-  });
-
   describe('verifyExistenceUsername', () => {
-    
+    beforeEach(() => {
+      ctx.request.body = mockJoinFields.username;
+    });
+
+    test('User 모델의 findAll 메서드를 호출해야 한다.', async () => {
+      await verifyExistenceUsername(ctx, next);
+      expect(User.findAll).toBeCalledWith(mockJoinFields.username);
+    });
+
+    test('상태코드 409로 응답해야 한다.', async () => {
+      await verifyExistenceUsername(ctx, next);
+      expect(ctx.status).toBe(409);
+    });
   });
 
   describe('sendSMSVerificationCode', () => {
-    
+    beforeEach(() => {
+      ctx.request.body = mockJoinFields.phoneNumber;
+    });
+
+    test('상태코드 200으로 응답해야 한다.', async () => {
+      await sendSMSVerificationCode(ctx, next);
+      expect(ctx.status).toBe(200);
+    });
+  });
+
+  describe('checkSMSVerificationCode', () => {
+    beforeEach(() => {
+      const mockVerificationCode = 123456;
+      ctx.request.body = mockVerificationCode;
+    });
+
+    test('상태코드 200으로 응답해야 한다.', async () => {
+      await checkSMSVerificationCode(ctx, next);
+      expect(ctx.status).toBe(200);
+    });
+  });
+
+  describe('createUser', () => {
+    beforeEach(() => {
+      ctx.request.body = mockJoinFields;
+    });
+
+    test('User 모델의 create 메서드를 호출해야 한다.', async () => {
+      await createUser(ctx, next);
+      expect(User.create).toBeCalledWith(mockJoinFields);
+    });
+
+    test('상태코드 201로 응답해야 한다.', async () => {
+      await createUser(ctx, next);
+      expect(ctx.status).toBe(201);
+    });
   });
 });
