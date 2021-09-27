ef429349c7206eb757e2dd3f21cbf44f099e43e8
baek dongjae <pandora2948@naver.com>
2021-09-27 20:17:11 +0900

    feat: 정규표현식 개선 및 인증코드 정규식 적용

--- client/app/lib/regex.js
+++ client/app/lib/regex.js
@@ -1,7 +1,8 @@
 const pattern = {
-  username: '[w.]{6,20}',
+  username: '[\\w]{6,20}',
   realname: '([가-힣]{2,20}|[a-zA-Z]{2,20})',
-  phoneNumber: '^(?=1)[d]{0,10}',
-  password: '[!@#$%^&*~+-w]{6,500}',
+  phoneNumber: '^\\d{10}',
+  password: '[!@#$%^&*~+-\\w]{6,500}',
+  verification: '\\d{6}',
 };
 export default pattern;
