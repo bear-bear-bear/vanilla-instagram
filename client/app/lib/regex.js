128233c905dfa8bd3a2bb4968ee839d435196974
baek dongjae <pandora2948@naver.com>
2021-09-28 11:30:39 +0900

    refactor: 정규식 오류 수정

--- client/app/lib/regex.js
+++ client/app/lib/regex.js
@@ -1,5 +1,5 @@
 const pattern = {
-  username: '[\\w]{6,20}',
+  username: '[\\w.]{6,20}',
   realname: '([가-힣]{2,20}|[a-zA-Z]{2,20})',
   phoneNumber: '^\\d{10}',
   password: '[!@#$%^&*~+-\\w]{6,500}',
