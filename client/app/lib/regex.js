90a6bc60345fe4b4b8ade5d1dea435d80c03f5bf
baek dongjae <pandora2948@naver.com>
2021-09-29 14:08:26 +0900

    recator: 불필요한 스크립트 파일 수정 및 pug를 사용하여 인풋 생성

--- client/app/lib/regex.js
+++ client/app/lib/regex.js
@@ -3,6 +3,5 @@ const pattern = {
   realname: '([가-힣]{2,20}|[a-zA-Z]{2,20})',
   phoneNumber: '^\\d{10}',
   password: '[!@#$%^&*~+-\\w]{6,500}',
-  verification: '\\d{6}',
 };
 export default pattern;
--- client/app/pages/verification/entry.js
+++ client/app/pages/verification/entry.js
@@ -2,4 +2,3 @@ import '@/templates/auth-container/_entry';
 import '@/templates/footer/_entry';
 
 import './verification.scss';
-import './verification.js';
