d97394d5aca6c2c2bba1a24030dd4909238d7bb6
galaxy4276 <galaxyhi4276@gmail.com>
2021-10-07 10:15:14 +0900

    feat: Swagger API 문서 작성 및 서버에 Swagger 적용 (#69)
    * npm: koa2-swagger-ui, yamljs 설치
    
    * feat: swagger 렌더 미들웨어 함수 작성 및 app 적용
    
    * feat: api swagger 문서 작성
    
    * modify: 이름 수정

---
+++ server/src/lib/setSwaggerMiddleware.ts
@@ -0,0 +1,15 @@
+import { join } from 'path';
+import Koa from 'koa';
+import yamljs from 'yamljs';
+import { koaSwagger } from 'koa2-swagger-ui';
+
+export default function setSwaggerMiddleware(app: Koa): void {
+  const spec = yamljs.load(join(__dirname, '..', '..', 'api.yaml'));
+  app.use(
+    koaSwagger({
+      routePrefix: '/',
+      title: 'Vanilla Instagram APIs',
+      swaggerOptions: { spec },
+    })
+  );
+}
